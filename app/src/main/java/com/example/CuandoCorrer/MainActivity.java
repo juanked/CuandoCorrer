5c09ecc8258c5ed23cffde243c9839a1490f4ad7
David Cristóbal <david@juanked.es>
2022-06-15 02:54:40 +0200

    Funciona la presentación de predicciones

--- app/src/main/java/com/example/CuandoCorrer/MainActivity.java
+++ app/src/main/java/com/example/CuandoCorrer/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.CuandoCorrer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -9,11 +11,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.example.CuandoCorrer.adapters.WeatherAdapter;
 import com.example.CuandoCorrer.helpers.current.WeatherResult;
 import com.example.CuandoCorrer.helpers.forecast.ForecastResult;
 import com.example.CuandoCorrer.helpers.remote.ApiUtils;
 import com.example.CuandoCorrer.helpers.remote.WeatherResponse;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
     private double nlon = -3.689978;
     private String lat = String.valueOf(nlat);
     private String lon = String.valueOf(nlon);
+    private WeatherAdapter adapter;
+    private WeatherResponse response = ApiUtils.getWeatherResponse();
+    private ForecastResult forecastResult;
+    //private ForecastResult forecastResult2;
+    private RecyclerView forecastList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +44,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 getCurrentData();
+                getForecast();
             }
         });
+        forecastList = (RecyclerView) findViewById(R.id.forecastRecyclerView);
+        getForecast();
+        forecastList.setLayoutManager(new LinearLayoutManager(this));
+        //getForecast();
+        //Log.d("forecast almacenado",forecastResult.toString());
+
+            adapter = new WeatherAdapter(forecastResult);
+            forecastList.setAdapter(adapter);
+
+
+
     }
 
     void getCurrentData() {
-        WeatherResponse response = ApiUtils.getWeatherResponse();
         Call<WeatherResult> callCurrent = response.getCurrentWeather(lat, lon, ApiKey, units);
         callCurrent.enqueue(new Callback<WeatherResult>() {
             @Override
@@ -53,6 +73,11 @@ public class MainActivity extends AppCompatActivity {
                             "Temperature: " + weatherResult.getMain().getTemp() + "\n" +
                                     "Wind: " + weatherResult.getWind().getSpeed() + "\n" +
                                     "Weather: " + weatherResult.getWeather().get(0).getMain();
+                    // TODO aparezcan datos en pantalla
+                    findViewById(R.id.txt_time)
+                   /* current.clear();
+                    current.put("temperature",weatherResult.getMain().getTemp().toString());
+                    current.put("wind",weatherResult.getWind().getSpeed().toString());*/
                     Log.i("Resultado: ", stringCurrent);
                 }
             }
@@ -62,16 +87,20 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
-
+    //public ForecastResult getForecast(){
+    public void getForecast(){
         Call<ForecastResult> callForecast = response.getForecast(lat, lon, ApiKey, units);
         callForecast.enqueue(new Callback<ForecastResult>() {
             @Override
             public void onResponse(Call<ForecastResult> call, Response<ForecastResult> response) {
                 if (response.code() == 200) {
-                    ForecastResult forecastResult = response.body();
-                    assert forecastResult != null;
 
+                    forecastResult = response.body();
+                    Log.d("Tamano forecastResult", forecastResult.getCnt().toString());
+                    assert forecastResult != null;
+                    //adapter.setForecastResult(forecastResult);
                     String stringForecast = null;
                     for (int i = 0; i < forecastResult.getCnt(); i++) {
                         stringForecast = stringForecast +
@@ -86,18 +115,30 @@ public class MainActivity extends AppCompatActivity {
                                 "\n";
                     }
                     Log.i("Resultado predicción", stringForecast);
-                }
+                    adapter.setForecastResult(forecastResult);
+                    /*try {
+                        adapter = new WeatherAdapter(forecastResult);
+                        forecastList.setAdapter(adapter);
+                        forecastList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                    } catch (Exception e ){
+                        Log.e("Fallo en try", e.getMessage());
+                    }*/
 
+                }
             }
 
             @Override
             public void onFailure(Call<ForecastResult> call, Throwable t) {
-                Log.e("Fallo2", t.getMessage() + "");
+                Log.e("Fallo", t.getMessage() + "");
 
             }
         });
-
-
+        //if (forecastResult2 == null)
+            //Log.d("Count forecast","Es nulo2222");
+        //return forecastResult2;
     }
-
+    /*public void setForecastResult (ForecastResult forecastResult2){
+        forecastResult = forecastResult2;
+        adapter.notifyDataSetChanged();
+    }*/
 }
--- app/src/main/java/com/example/CuandoCorrer/adapters/WeatherAdapter.java
+++ app/src/main/java/com/example/CuandoCorrer/adapters/WeatherAdapter.java
@@ -1,8 +1,136 @@
 package com.example.CuandoCorrer.adapters;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.icu.util.TimeZone;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.CuandoCorrer.R;
+import com.example.CuandoCorrer.helpers.forecast.ForecastResult;
+import com.squareup.picasso.Picasso;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.util.Date;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
-/*public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHolder> {
+public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHolder> {
 
-    private List<>
-}*/
+    private ForecastResult forecastResult;
+    private Context context;
+    private String image;
+    private int userTemp = 20;
+    private int userWind = 20;
+    private boolean userRain = false;
+    public WeatherAdapter(ForecastResult forecastResult){
+        if (this.forecastResult == null)
+            Log.d("Count forecast","Es nulo");
+        this.forecastResult = forecastResult;
+    }
+
+    public void setForecastResult(ForecastResult forecastResult){
+        this.forecastResult = forecastResult;
+        notifyDataSetChanged();
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View convertView = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.item_list, parent, false);
+        return new ViewHolder(convertView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Double temp = forecastResult.getList().get(position).getMain().getTemp();
+        Log.i("forecastResult",temp.toString());
+        Double wind = forecastResult.getList().get(position).getWind().getSpeed();
+        Log.i("forecastResult",wind.toString());
+        String weather = forecastResult.getList().get(position).getWeather().get(0).getMain();
+        Log.i("forecastResult",weather);
+        int time = forecastResult.getList().get(position).getDt();
+        if (temp > userTemp && wind < userWind && (weather.equals("Rain") == userRain)){
+            holder.card.setBackgroundColor(ContextCompat.getColor(holder.card.getContext(),R.color.good_temp));
+        } else {
+            holder.card.setBackgroundColor(ContextCompat.getColor( holder.card.getContext(),R.color.bad_temp));
+        }
+        String tempP = temp.toString()+"ºC";
+        holder.temperature.
+                setText(tempP);
+        String windP = wind.toString();
+        holder.wind.
+                setText(windP);
+        holder.weather.
+                setText(weather);
+        //String timeP = String.valueOf(time);
+        holder.time.setText(toDate(time,holder.card.getContext()));
+        image = forecastResult.getList().get(position).getWeather().get(0).getIcon();
+        Picasso.get().load("https://openweathermap.org/img/wn/"+image+"@2x.png").
+                resize(100,100).into(holder.imWeather);
+    }
+
+    @Override
+    public int getItemCount() {
+        //return forecastResult.getCnt();
+        //return 0;
+
+        return  forecastResult == null ? 0 : forecastResult.getCnt();
+    }
+
+    /*public View getView (int position, View convertView, ViewGroup parent){
+        ViewHolder vh;
+        if (convertView == null){
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_list,null);
+            vh = new ViewHolder();
+            vh.temperature = convertView.findViewById(R.id.txt_temperature);
+            vh.wind = convertView.findViewById(R.id.txt_wind);
+            vh.weather = convertView.findViewById(R.id.txt_weather);
+            vh.imWeather = convertView.findViewById(R.id.img_imWeather);
+            convertView.setTag(vh);
+        } else {
+            vh = (ViewHolder) convertView.getTag();
+        }
+
+        ForecastResult forecastResult = forecastResultList.get(position);
+
+        return convertView;
+    }*/
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView temperature;
+        private TextView wind;
+        private TextView weather;
+        private TextView time;
+        private ImageView imWeather;
+        private View card;
+        public ViewHolder (@NonNull View itemView){
+            super(itemView);
+            temperature = (TextView) itemView.findViewById(R.id.txt_temperature);
+            wind = (TextView) itemView.findViewById(R.id.txt_wind);
+            weather = (TextView) itemView.findViewById(R.id.txt_weather);
+            time = (TextView) itemView.findViewById(R.id.txt_time);
+            imWeather = (ImageView) itemView.findViewById(R.id.img_imWeather);
+            card = itemView.findViewById(R.id.card_view_item);
+        }
+    }
+
+    private String toDate (int unix_time,Context context){
+        Date date = new Date(unix_time*1000L);
+// the format of your date
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+        DateFormat dateFormat = android.text.format.DateFormat.getTimeFormat(context.getApplicationContext());
+        String time = dateFormat.format(date);
+        return time;
+
+    }
+}
